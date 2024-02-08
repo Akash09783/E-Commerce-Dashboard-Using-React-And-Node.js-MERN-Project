ab838beb5451ee1a484d9951229aefba1753434d
Akash <kumawat.aakash515@gmail.com>
2024-02-08 16:27:19 +0530

    sendind data through api

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,26 @@
 const express = require("express");
-
-const mongoose = require('mongoose')
+const User = require('./db/User')
+require('./db/config')
 const app = express();
-const connectDB = async ()=>{
-  mongoose.connect("mongodb://localhost:27017/e-dashboard")
-const productSchema = new mongoose.Schema({})
-  const product = mongoose.model('product',productSchema);
-  const data = await product.find();
-  console.warn(data)
 
-}
-connectDB();
+app.use(express.json())
+// const connectDB = async ()=>{
+//   mongoose.connect("mongodb://localhost:27017/e-dashboard")
+// const productSchema = new mongoose.Schema({})
+//   const product = mongoose.model('product',productSchema);
+//   const data = await product.find();
+//   console.warn(data)
+
+// }
+// connectDB();
+
+app.post('/register',async(req,res)=>{
+  let user = new User(req.body);
+  let result =await user.save()
+  res.send(result)
+
+
+})
 
 
 app.listen(5000);
