4669fb5e82e9e62214ffdae662d0a1ef45073cf4
Akash <kumawat.aakash515@gmail.com>
2024-02-08 13:44:27 +0530

    routing for navbar done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Nav from './Nav';
+import {HashRouter,Routes,Route} from 'react-router-dom'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+
+  <div className="App">
+  <HashRouter>
+
+  <Nav/>
+  <Routes>
+<Route path='/' element={<h1>Product Listing Component</h1>} />
+<Route path='/add' element={<h1>Add Product Component</h1>} />
+<Route path='/update' element={<h1>Update Product Component</h1>} />
+<Route path='/logout' element={<h1>LogOut Component</h1>} />
+<Route path='/profile' element={<h1>Profile Component</h1>} />
+  </Routes>
+ 
+  </HashRouter>
+
+
+  </div>
+
+  </>
   );
 }
 
