9750b94314f265493e7c25de067e6e83d385449c
Akash <kumawat.aakash515@gmail.com>
2024-02-10 12:48:51 +0530

    update Api integration done

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const User = require("./db/User");
 require("./db/config");
 const app = express();
 const cors = require("cors");
-const Product = require('./db/Product')
+const Product = require("./db/Product");
 app.use(express.json());
 app.use(cors());
 // const connectDB = async ()=>{
@@ -16,69 +16,68 @@ app.use(cors());
 // }
 // connectDB();
 
-
 //register
 app.post("/register", async (req, res) => {
   let user = new User(req.body);
   let result = await user.save();
-result = result.toObject();
-delete result.password;
+  result = result.toObject();
+  delete result.password;
   res.send(result);
 });
- //login 
+//login
 app.post("/login", async (req, res) => {
-  console.log(req.body)
-  if(req.body.password && req.body.email){
+  console.log(req.body);
+  if (req.body.password && req.body.email) {
     let user = await User.findOne(req.body).select("-password");
     if (user) {
       res.send(user);
     } else {
       res.send({ result: "no user found" });
     }
-  }else{
-    res.send({result:"no user"})
+  } else {
+    res.send({ result: "no user" });
   }
-
 });
 
 //add product
 
-app.post("/add-product",async(req,res)=>{
-  let product = new Product(req.body)
+app.post("/add-product", async (req, res) => {
+  let product = new Product(req.body);
   let result = await product.save();
-  res.send(result)
-
-})
+  res.send(result);
+});
 //product list
-app.get("/products",async(req,res)=>{
+app.get("/products", async (req, res) => {
   let products = await Product.find();
-if(products.length>0){
-  res.send(products)
-}else{
-  res.send({result:"no one"})
-}
+  if (products.length > 0) {
+    res.send(products);
+  } else {
+    res.send({ result: "no one" });
+  }
+});
 
-})
-
-//delete product 
-app.delete('/product/:id',async(req,res)=>{
-  const result =await Product.deleteOne({_id:req.params.id})
-res.send(result)
-
-})
+//delete product
+app.delete("/product/:id", async (req, res) => {
+  const result = await Product.deleteOne({ _id: req.params.id });
+  res.send(result);
+});
 
 // update product
 
-app.get("/product/:id",async(req,res)=>{
-let result = await Product.findOne({_id:req.params.id})
+app.get("/product/:id", async (req, res) => {
+  let result = await Product.findOne({ _id: req.params.id });
 
-  res.send(result)
+  res.send(result);
+});
 
-
-})
-
-
-
-
+app.put("/productt/:id", async (req, res) => {
+  let result = await Product.updateOne(
+    { _id: req.params.id },
+    {
+      $set: req.body,
+    }
+  );
+  res.send(result);
+});
 
 app.listen(5000);
--- src/components/UpdateProduct.js
+++ src/components/UpdateProduct.js
@@ -1,32 +1,41 @@
 import React, { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams,useNavigate } from "react-router-dom";
 
 const UpdateProduct = () => {
   const [name, setName] = React.useState("");
   const [price, setPrice] = React.useState("");
   const [category, setCategory] = React.useState("");
   const [company, setCompany] = React.useState("");
-const params = useParams();
-useEffect(()=>{
-   
-  console.warn(params)
-  getProductDetails()
-})
+  const params = useParams();
 
-const getProductDetails = async ()=>{
+  const navigate = useNavigate();
+  useEffect(() => {
+    getProductDetailss();
+  }, []);
 
-    let result  = await fetch(`http://localhost:5000/product/${params._id}`);
-result = await result.json()
-setName(result.name)
-setPrice(result.price)
-setCategory(result.category)
-setCompany(result.company)
+  const getProductDetailss = async () => {
+    console.warn(params);
+    let result = await fetch(`http://localhost:5000/product/${params._id}`);
+    result = await result.json();
+    setName(result.name);
+    setPrice(result.price);
+    setCategory(result.category);
+    setCompany(result.company);
+  };
 
-}
-
-  const UpdateProduct=()=>{
-    console.warn(name,price,category,company)
-  }
+  const UpdateProduct = async () => {
+    console.warn(name, price, category, company);
+    let result = await fetch(`http://localhost:5000/productt/${params._id}`, {
+      method: "put",
+      body: JSON.stringify({ name, price, category, company }),
+      headers: {
+        "Content-Type": "application/json",
+      }
+    });
+    result = await result.json();
+    console.warn(result); 
+    navigate('/')
+  };
   return (
     <div
       className="addProduct"
@@ -34,12 +43,11 @@ setCompany(result.company)
     >
       <h1
         style={{
-          
           marginRight: "0",
           fontFamily: "sans-serif",
-          fontStyle: 'inherit',
-          fontSize:'30px',
-          marginLeft:'12%'
+          fontStyle: "inherit",
+          fontSize: "30px",
+          marginLeft: "12%",
         }}
       >
         Update Product
@@ -62,7 +70,7 @@ setCompany(result.company)
         className="inputBox"
         placeholder="Enter Product Price"
       />
-       <input
+      <input
         value={category}
         onChange={(e) => {
           setCategory(e.target.value);
